fe18eedd1048eca098c9984bc22f25df9c08eb10
Rexhino Kovaci <rexhino.kovaci@jkmoving.com>
2024-09-12 19:43:58 +0200

    Added Loadtest results

--- loadtest/loadtest.js
+++ loadtest/loadtest.js
@@ -4,8 +4,8 @@ import { check, sleep } from 'k6';
 export const options = {
   stages: [
     { duration: '1m', target: 1000 },
-    { duration: '5m', target: 1000 }, 
-    { duration: '1m', target: 0 },
+    { duration: '5m', target: 1000 },
+    { duration: '1m', target: 0 },  
   ],
 };
 
